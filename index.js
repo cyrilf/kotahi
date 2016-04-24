a35ebb7779ea65fc47680af5138c9aee910283fa
cyrilf <cyrilf7x@gmail.com>
2016-04-24 23:44:31 +1200

    Handle the message reception from Facebook

--- index.js
+++ index.js
@@ -15,7 +15,20 @@ app.get('/webhook/', (req, res) => {
   }
 
   res.send('You tried, you failed.');
-})
+});
+
+app.post('/webhook/', (req, res) => {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+    }
+  }
+  res.sendStatus(200);
+});
 
 app.listen(app.get('port'), function () {
   console.log('Kotahi ready at ' + app.get('port'));
