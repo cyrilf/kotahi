ec9216474a1bce154b2bd275a2d3b5892cdacbe3
cyrilf <cyrilf7x@gmail.com>
2016-04-25 00:54:24 +1200

    Fix all code indentation mistakes

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ app.set('port', (process.env.PORT || 2377));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send("I'm Kotahi, let's play!");
+    res.send("I'm Kotahi, let's play!");
 });
 
 app.get('/webhook/', (req, res) => {
-  if (req.query['hub.verify_token'] === 'moemoea_NZ') {
-    res.send(req.query['hub.challenge']);
-  }
+    if (req.query['hub.verify_token'] === 'moemoea_NZ') {
+        res.send(req.query['hub.challenge']);
+    }
 
-  res.send('You tried, you failed.');
+    res.send('You tried, you failed.');
 });
 
 let players = [];
@@ -66,41 +66,41 @@ function handleMessage(senderId, text) {
 }
 
 function sendTextMessage(sender, text) {
-  const messageData = {
-    text: text,
-};
+    const messageData = {
+        text: text,
+    };
 
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: token },
-    method: 'POST',
-    json: {
-      recipient: { id: sender },
-      message: messageData,
-    }
-}, (error, response, body) => {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: token },
+        method: 'POST',
+        json: {
+            recipient: { id: sender },
+            message: messageData,
+        }
+    }, (error, response, body) => {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
 }
 
 app.post('/webhook/', (req, res) => {
-  const messaging_events = req.body.entry[0].messaging;
-  for (let i = 0; i < messaging_events.length; i++) {
-    const event = req.body.entry[0].messaging[i];
-    const sender = event.sender.id;
-    if (event.message && event.message.text) {
-      let text = event.message.text;
-      handleMessage(sender, text);
+    const messaging_events = req.body.entry[0].messaging;
+    for (let i = 0; i < messaging_events.length; i++) {
+        const event = req.body.entry[0].messaging[i];
+        const sender = event.sender.id;
+        if (event.message && event.message.text) {
+            let text = event.message.text;
+            handleMessage(sender, text);
+        }
     }
-  }
 
-  res.sendStatus(200);
+    res.sendStatus(200);
 });
 
 app.listen(app.get('port'), function () {
-  console.log('Kotahi ready at ' + app.get('port'));
+    console.log('Kotahi ready at ' + app.get('port'));
 });
