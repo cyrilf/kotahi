ccdf36239e9eed1b7b8839e617632b8ab9ebe880
cyrilf <cyrilf7x@gmail.com>
2016-04-24 23:40:02 +1200

    Setup the webhook route for Facebook

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ let app = express();
 
 app.set('port', (process.env.PORT || 2377));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send("I'm Kotahi, let's play!");
 });
+
+app.get('/webhook/', (req, res) => {
+  if (req.query['hub.verify_token'] === 'moemoea_NZ') {
+    res.send(req.query['hub.challenge']);
+  }
+
+  res.send('You tried, you failed.');
+})
 
 app.listen(app.get('port'), function () {
   console.log('Kotahi ready at ' + app.get('port'));
